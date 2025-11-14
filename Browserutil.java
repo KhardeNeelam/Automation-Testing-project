4ffa3af2debead00bb815f9a4e16074aefc8d016
KhardeNeelam <113774219+KhardeNeelam@users.noreply.github.com>
2025-11-14 22:22:30 +0800

    Enhance Browserutil with method documentation
    Added documentation for the Browserutil class and its method.

--- Browserutil.java
+++ Browserutil.java
@@ -4,31 +4,41 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
+/**
+ * Utility class for creating browser driver instances.
+ */
 public class Browserutil {
-	
+	/**
+     * Returns a RemoteWebDriver instance for the specified browser name.
+     * Supported browsers: "chrome", "firefox", "edge".
+     * Defaults to ChromeDriver if browserName is null or unrecognized.
+     *
+     * @param browserName the name of the browser ("chrome", "firefox", "edge")
+     * @return RemoteWebDriver instance
+     */
 	public static RemoteWebDriver getDriver(String browserName) {
 		RemoteWebDriver driver;
-		
+		// Default to ChromeDriver if browserName i
 		if (browserName == null) {
 			driver = new ChromeDriver();
-		}
+		} // Create ChromeDriver if browserName is "chrome"
 		else if (browserName.equalsIgnoreCase("chrome")){
 			driver = new ChromeDriver();
 		}
-		
+		// Create FirefoxDriver if browserName is "firefox"
 		else if (browserName.equalsIgnoreCase("firefox")){
 			driver = new FirefoxDriver();
-		}
+		}         // Create EdgeDriver if browserName is "edge"
 		else if (browserName.equalsIgnoreCase("edge")){
 			driver = new EdgeDriver();
 		}
-		
+		// Default to ChromeDriver for any other values
 		else {
 			driver = new ChromeDriver();
 		}
-		
+		// Maximize the browser window before returning the driver
 		driver.manage().window().maximize();
 		return driver;
 	}
 }
+

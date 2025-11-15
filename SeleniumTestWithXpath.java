159a7d99399c595523a3717acc24648bcdeaa7f6
KhardeNeelam <113774219+KhardeNeelam@users.noreply.github.com>
2025-11-15 21:19:45 +0800

    Refactor Selenium test with improved comments
    Updated comments and added class-level documentation for clarity.

--- SeleniumTestWithXpath.java
+++ SeleniumTestWithXpath.java
@@ -3,7 +3,10 @@ package tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+/**
+ * This class demonstrates basic Selenium WebDriver operations using ChromeDriver.
+ * It performs actions on Google and Facebook websites using different element locators.
+ */
 public class SeleniumTestWithXpath {
 
 	public static void main(String[] args) {
@@ -11,17 +14,21 @@ public class SeleniumTestWithXpath {
 		ChromeDriver driver = new ChromeDriver();
 		//FirefoxDriver	driver = new FirefoxDriver();
 		//EdgeDriver driver = new EdgeDriver();
+		// Open Google homepage
 		driver.get("https://www.google.com");
-		
+		// Find the search text area using an incorrect selector: By.cssSelector with an XPath string
+		// (Should be: By.xpath or use valid CSS selector)
 		WebElement textArea = driver.findElement(By.cssSelector("//textarea[name='q']"));
+		// Type "Selenium Dev" into the search text area
 		textArea.sendKeys("Selenium Dev");
 	/*	WebElement button = driver.findElement(By.className("gNO89b"));
 		button.click();*/
-		
+		// Navigate to Facebook homepage
 		driver.navigate().to("https://www.facebook.com");
 		driver.findElement(By.cssSelector("input#email")).sendKeys("neelam@gmail.com");
 		//driver.findElement(By.tagName("button")).click();
 		//driver.findElement(By.linkText("Forgotten password?")).click();
+		// End of test actions
 		
 		
 		
@@ -29,3 +36,4 @@ public class SeleniumTestWithXpath {
 	}
 
 }
+
